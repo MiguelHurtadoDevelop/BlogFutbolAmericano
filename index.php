13cb6f0da7706e2c5714ce62730a4556e0807fa0
MiguelHurtado <mhurtadojunior@gmail.com>
2023-12-11 15:12:32 +0100

    uy

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once "./Config/config.php";
 
 $dotenv= Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
-use Views\Layout\header;
+
 use Controllers\FrontController;
 FrontController::main();
 
